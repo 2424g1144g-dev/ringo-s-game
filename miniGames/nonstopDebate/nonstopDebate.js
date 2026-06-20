18c55f9b53e967c5772f138b25cd7f93f988df5f
2424g1144g-dev <2424g1144g@gse.okayama-c.ed.jp>
2026-06-20 11:28:54 +0900

    　

--- miniGames/nonstopDebate/nonstopDebate.js
+++ miniGames/nonstopDebate/nonstopDebate.js
@@ -45,7 +45,7 @@ window.addBullet = function(bulletText) {
   });
 }
 
-let bulletNum = ["ツカゴエのポケットに入っていたゴミ","１００年前の西大寺高校のビジョン","黒幕の罠"];
+let bulletNum = ["ツカゴエのポケットに入っていたゴミ","１００年前の西大寺高校のビジョン","黒幕の罠","消えた体育倉庫のハンマー","西大寺高校絶望的事件","シノハラの証言","割れたコップ"];
 function loadAllBullets() {
   bulletNum.forEach((text, i) => {
     setTimeout(() => {
