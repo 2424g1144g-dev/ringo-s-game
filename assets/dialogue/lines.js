b7c1ab1123a188f9c9a8067db2dc0571cfcbe9b3
2424g1144g-dev <2424g1144g@gse.okayama-c.ed.jp>
2026-08-29 11:04:49 +0900

    　

--- assets/dialogue/lines.js
+++ assets/dialogue/lines.js
@@ -19,8 +19,8 @@ DIALOGUE_LINES.description = [
   { name: "ニシモト　タツロウ", lines: [{segments: [{text: "退職させられたのは"},{text: "オカモトユウダイ先生", class: "yellow"},{text: "でした..."}], event: [{type: "diaShowToggle"},{type: "textEnterChange", id: true}]}]},
   { name: " ", lines: [{segments: [{text: " "}], event: [{type: "diaShowToggle"},{type: "fileShowToggle"},{type: "textEnterChange", id: false}], autoNext: true, dialogueAutoNext: true, wait: 2000}]},
   { name: "ニシモト　タツロウ", lines: [{segments: [{text: "現場は体育館..."},{text: "サイコウファイル", class: "yellow"},{text: "によると..."}], event: [{type: "diaShowToggle"},{type: "textEnterChange", id: true}]}]},
-  { name: "ニシモト　タツロウ", lines: [{segments: [{text: "退職時刻は"},{text: "22:00", class: "yellow"},{text: "...ちょうどみんなが職員室にいたときですね。"}]},
-                                        {segments: [{text: "凶器は"},{text: "鈍器", class: "yellow"},{text: "...これで気絶させられた後に退職ですか..."}]}]}
+  { name: "ニシモト　タツロウ", lines: [{segments: [{text: "退職時刻は"},{text: "22:00", class: "yellow"},{text: "...　ちょうどみんなが職員室にいたときですね。"}]},
+                                        {segments: [{text: "凶器は"},{text: "鈍器", class: "yellow"},{text: "...　これで気絶させられた後に退職ですか..."}]}]}
 ]
 
 DIALOGUE_LINES.dialogueStart = [
